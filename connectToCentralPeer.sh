c261ae776d827e997a7603efa0eda1cd8f74ca61
Koen van der Kruk <koenvdkruk@gmail.com>
2015-08-23 15:24:21 +0200

    Added more shell scripts

---
+++ connectToCentralPeer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo sshfs -o allow_other userPeer1@192.168.178.27:/home/koen/gitBareRepoEncrypted /home/koen/fileCo1/remoteGit
---
+++ disconnectCentralPeer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo umount /home/koen/fileCo1/remoteGit
