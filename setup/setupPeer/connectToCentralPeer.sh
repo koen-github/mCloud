afa6e49894fe0941b1fd4d50c8c194eee42e2d33
Koen van der Kruk <koenvdkruk@gmail.com>
2015-09-06 16:01:34 +0200

    small change

--- setup/setupPeer/connectToCentralPeer.sh
+++ setup/setupPeer/connectToCentralPeer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 USER=`whoami`
 IP=$1
-sudo sshfs -o allow_other $USER@$IP:/home/koen/gitBareRepoEncrypted /home/$USER/fileCo1/remoteGit
+sudo sshfs -o allow_other $USER@$IP:/home/koen/gitBareRepoEncrypted /home/$USER/fileCo1/remoteGit -o IdentityFile=/home/$USER/.ssh/id_rsa
