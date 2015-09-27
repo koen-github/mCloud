373f7863f1a957dd5f53b96d3c775496c06b0cf1
Koen van der Kruk <koenvdkruk@gmail.com>
2015-09-27 18:00:07 +0200

    Server side changes

--- setup/central_peer_scripts/add_Script_to_sudoers.sh
+++ setup/central_peer_scripts/add_Script_to_sudoers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "WARNING, THIS SCRIPT MUST BE RUN AS ROOT"
-ADD_USER_TO_REPO_LOCATION="/home/koen/mCloud/setup/central_peer_scripts/addUserToRepo.sh"
+ADD_USER_TO_REPO_LOCATION="`pwd`/addUserToRepo.sh"
 echo "This script will add this line to the sudoers file: "
 echo "%users ALL=NOPASSWD: $ADD_USER_TO_REPO_LOCATION"
 echo "So normal 'users' can run the addUserToRepo script without being asked for a sudo password"
