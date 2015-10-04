4be5aec75b0afa5ab554c72ee1758041923a2a5f
Koen van der Kruk <koenvdkruk@gmail.com>
2015-10-04 15:30:37 +0200

    Fix for connection reset by peer

--- connectToCentralPeer.sh
+++ connectToCentralPeer.sh
@@ -2,7 +2,7 @@
 FILE_CONTENTS=`cat LOCAL_CONFIG`
 eval $FILE_CONTENTS
 echo $CENTRAL_IP
-sudo sshfs -o allow_other $USER@$CENTRAL_IP:../../ $LOCATION_SSHFS_MOUNTPOINT -o IdentityFile=$USER_RSA_FILE 
+sudo sshfs -o allow_other $USER@$CENTRAL_IP:../../ $LOCATION_SSHFS_MOUNTPOINT -o IdentityFile=$USER_RSA_FILE  -p 22 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
 
 
 
