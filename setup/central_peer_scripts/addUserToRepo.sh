a1742ee8468f863c6c039a1b7da1cf9006e75495
Koen van der Kruk <koenvdkruk@gmail.com>
2015-10-04 16:06:36 +0200

    User to Repo run from peer

--- setup/central_peer_scripts/addUserToRepo.sh
+++ setup/central_peer_scripts/addUserToRepo.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 BareFileCo="/home/koen/gitBareRepoEncrypted"
+echo "Use this script as ./addUserToRepo.sh REPO_NAME LIST_OF_USERS_SEPERATED_BY;"
+echo "Or open and follow steps"
 echo "Warning, this script must be run as root, or use the sudoers script"
 echo "Warning, git repo must be in the top level directory of the main git central"
+if [ -z $1 ]
+then
+
 echo "Name of the repo"
 read REPO_NAME
 
+else
+REPO_NAME=$1
+fi
+
+if [ -z $2 ]
+then
 echo "Users, seperated by ; without spaces"
 read USERS
+else
+USERS=$2
+fi
+
+
 
 groupadd group_$REPO_NAME
 
