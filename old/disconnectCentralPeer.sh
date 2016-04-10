add8c76111c4a5a160ab976c775f9524376186e6
Koen van der Kruk <koen@debian>
2016-04-10 16:40:02 +0200

    Starting to create debian package for mCloud, including all questions and options

--- old/disconnectCentralPeer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-FILE_CONTENTS=`cat ~/LOCAL_CONFIG`
-eval $FILE_CONTENTS
-
-sudo umount $LOCATION_SSHFS_MOUNTPOINT
--- old/openEncryptedImage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-FILE_CONTENTS=`cat ~/LOCAL_CONFIG`
-eval $FILE_CONTENTS
-echo $IMAGE_FILE
-
-sudo cryptsetup luksOpen $IMAGE_FILE $LOCAL_FILE_CONTAINER --key-file $KEY_FILE
-
-sudo mount /dev/mapper/$LOCAL_FILE_CONTAINER $LOCATION_MOUNTPOINT
-
